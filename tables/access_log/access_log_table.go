aa615e56b708a9b56673a4613be6a55915c5bf1e
Graza <code@graza.io>
2025-04-01 10:05:17 +0100

    refactor: updated to use RowErrors (#6)

--- tables/access_log/access_log_table.go
+++ tables/access_log/access_log_table.go
@@ -4,6 +4,7 @@ import (
 	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/tailpipe-plugin-sdk/artifact_source"
 	"github.com/turbot/tailpipe-plugin-sdk/constants"
+	"github.com/turbot/tailpipe-plugin-sdk/error_types"
 	"github.com/turbot/tailpipe-plugin-sdk/formats"
 	"github.com/turbot/tailpipe-plugin-sdk/row_source"
 	"github.com/turbot/tailpipe-plugin-sdk/schema"
@@ -38,124 +39,124 @@ func (c *AccessLogTable) GetTableDefinition() *schema.TableSchema {
 			// default format fields
 			{
 				ColumnName: "remote_addr",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "remote_logname",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "remote_user",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "timestamp",
-				Type:       "TIMESTAMP", // TODO: Confirm column type
+				Type:       "timestamp",
 			},
 			{
 				ColumnName: "request_method",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "request_uri",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "server_protocol",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "status",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "body_bytes_sent",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "http_referer",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "http_user_agent",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			// additional fields
 			{
 				ColumnName: "local_addr",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "filename",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "keepalive_requests",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "server_name",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "server_port",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "client_port",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "apache_port",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "pid",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "thread_id",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "hex_thread_id",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "query_string",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "handler",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "request_time",
-				Type:       "FLOAT",
+				Type:       "float",
 			},
 			{
 				ColumnName: "request_time_ms",
-				Type:       "FLOAT",
+				Type:       "float",
 			},
 			{
 				ColumnName: "request_time_us",
-				Type:       "FLOAT",
+				Type:       "float",
 			},
 			{
 				ColumnName: "connection_status",
-				Type:       "VARCHAR",
+				Type:       "varchar",
 			},
 			{
 				ColumnName: "bytes_received",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "bytes_sent",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 			{
 				ColumnName: "bytes_transferred",
-				Type:       "INTEGER",
+				Type:       "integer",
 			},
 		},
 		NullValue: "-", // default null value
@@ -186,9 +187,26 @@ func (c *AccessLogTable) EnrichRow(row *types.DynamicRow, sourceEnrichmentFields
 	if ts, ok := row.GetSourceValue("timestamp"); ok && ts != AccessLogTableNilValue {
 		t, err := helpers.ParseTime(ts)
 		if err != nil {
-			return nil, err
+			return nil, error_types.NewRowErrorWithFields([]string{}, []string{"timestamp"})
 		}
 		row.OutputColumns[constants.TpTimestamp] = t
+	}
+
+	// tp_ips
+	var ips []string
+	if ip, ok := row.GetSourceValue("remote_addr"); ok && ip != AccessLogTableNilValue {
+		ips = append(ips, ip)
+	}
+	if ip, ok := row.GetSourceValue("local_addr"); ok && ip != AccessLogTableNilValue {
+		ips = append(ips, ip)
+	}
+	if len(ips) > 0 {
+		row.OutputColumns[constants.TpIps] = ips
+	}
+
+	// tp_usernames
+	if username, ok := row.GetSourceValue("remote_user"); ok && username != AccessLogTableNilValue {
+		row.OutputColumns[constants.TpUsernames] = []string{username}
 	}
 
 	// now call the base class to do the rest of the enrichment
